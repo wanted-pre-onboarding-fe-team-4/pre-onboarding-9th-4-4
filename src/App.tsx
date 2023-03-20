70164079addf601954bcee791a7b8d6e08f7dc21
Wooseong Kim <kimwooseong8313@gmail.com>
2023-03-21 01:54:19 +0900

    feat: Table props 제거, data 전역 객체 참조

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ export function App() {
 
   return (
     <div>
-      <Table data={data} />
+      <Table />
     </div>
   );
 }
--- src/components/table/index.tsx
+++ src/components/table/index.tsx
@@ -1,17 +1,15 @@
+import { useQuery } from 'react-query';
+import { getTableData } from '../../apis/api';
 import { ITableData } from '../../types/ITableData';
-
-interface TableProps {
-  data: ITableData[];
-}
 
 const TODAY = '2023-03-08';
 const todayFilter = (record: ITableData): boolean => {
   return record.transaction_time.includes(TODAY);
 };
 
-export const Table = ({ data }: TableProps) => {
-  const filteredData = data.filter((record) => todayFilter(record));
+export const Table = () => {
+  const { data } = useQuery('getTableData', getTableData);
+  const filteredData = data?.filter((record) => todayFilter(record));
   console.log(filteredData);
-
   return <></>;
 };
