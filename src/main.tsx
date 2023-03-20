8ad42431b4022b85f858fe85d7516cd4e9df6c9d
Wooseong Kim <kimwooseong8313@gmail.com>
2023-03-21 00:42:19 +0900

    feat: 서스펜스, 데이터 갱신을 위한 리액트 쿼리 구현

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,20 @@
+import { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { App } from './App';
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <App />
+  <QueryClientProvider client={queryClient}>
+    <Suspense
+      fallback={
+        <div>
+          <h1>Loading...</h1>
+        </div>
+      }
+    >
+      <App />
+    </Suspense>
+  </QueryClientProvider>
 );
