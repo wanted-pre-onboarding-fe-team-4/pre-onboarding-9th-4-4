3506027c83136c9e0582b96da204334e37c38449
dlxotjde <dlxotjde@daum.net>
2023-03-21 21:59:38 +0900

    feat sort

--- src/components/table/units/Columns.ts
+++ src/components/table/units/Columns.ts
@@ -7,11 +7,15 @@ export const COLUMNS: ColumnsType<ITableData> = [
     title: '주문번호',
     dataIndex: 'id',
     key: 'id',
+    sorter: (a, b) => a.id - b.id,
+    sortDirections: ['descend'],
   },
   {
     title: '거래시간',
     dataIndex: 'transaction_time',
     key: 'transaction_time',
+    sorter: (a:ITableData, b:ITableData) => Number(new Date(a.transaction_time)) - Number(new Date(b.transaction_time)),
+    sortDirections: ['descend'],
   },
   {
     title: '주문처리상태',
@@ -30,7 +34,8 @@ export const COLUMNS: ColumnsType<ITableData> = [
     ],
     filterMode: 'tree',
     filterSearch: true,
-    onFilter: (value: string | number | boolean, record: ITableData) => record.status === value,
+    onFilter: (value: string | number | boolean, record: ITableData) =>
+      record.status === value,
   },
   {
     title: '고객번호',
