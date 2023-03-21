4dba1507e2e4a8689e23621e805fcb06723ef323
dlxotjde <dlxotjde@daum.net>
2023-03-21 22:59:37 +0900

    feat Option objects && type

--- src/components/table/units/Columns.ts
+++ src/components/table/units/Columns.ts
@@ -1,41 +1,26 @@
 import { ColumnsType } from 'antd/es/table';
 import { ITableData } from '../../../types/ITableData';
-import { StatusRenderer } from './ColumnsOptions';
+import { filterOptionObject, sorterIdOptionObject, sorterTransactionTimeOptionObject, StatusRenderer } from './ColumnsOptions';
 
 export const COLUMNS: ColumnsType<ITableData> = [
   {
     title: '주문번호',
     dataIndex: 'id',
     key: 'id',
-    sorter: (a, b) => a.id - b.id,
-    sortDirections: ['descend'],
+    ...sorterIdOptionObject
   },
   {
     title: '거래시간',
     dataIndex: 'transaction_time',
     key: 'transaction_time',
-    sorter: (a:ITableData, b:ITableData) => Number(new Date(a.transaction_time)) - Number(new Date(b.transaction_time)),
-    sortDirections: ['descend'],
+    ...sorterTransactionTimeOptionObject
   },
   {
     title: '주문처리상태',
     dataIndex: 'status',
     key: 'status',
     render: StatusRenderer,
-    filters: [
-      {
-        text: '완료',
-        value: true,
-      },
-      {
-        text: '미완료',
-        value: false,
-      },
-    ],
-    filterMode: 'tree',
-    filterSearch: true,
-    onFilter: (value: string | number | boolean, record: ITableData) =>
-      record.status === value,
+    ...filterOptionObject
   },
   {
     title: '고객번호',
