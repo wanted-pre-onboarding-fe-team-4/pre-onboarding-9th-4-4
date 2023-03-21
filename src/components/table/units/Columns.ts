486b24ed546ecb693ac8e8500db6e93824c2dfa0
Taesung Lee <66891085+2taesung@users.noreply.github.com>
2023-03-21 23:23:35 +0900

    [#12] sort, filter extract to ColumnsOptions.ts (#15)

--- src/components/table/units/Columns.ts
+++ src/components/table/units/Columns.ts
@@ -1,36 +1,27 @@
 import { ColumnsType } from 'antd/es/table';
 import { ITableData } from '../../../types/ITableData';
-import { StatusRenderer } from './ColumnsOptions';
+import { filterOptionObject, sorterIdOptionObject, sorterTransactionTimeOptionObject, StatusRenderer } from './ColumnsOptions';
 
 export const COLUMNS: ColumnsType<ITableData> = [
   {
     title: '주문번호',
     dataIndex: 'id',
     key: 'id',
+    ...sorterIdOptionObject
   },
   {
     title: '거래시간',
     dataIndex: 'transaction_time',
     key: 'transaction_time',
+    ...sorterTransactionTimeOptionObject
   },
   {
     title: '주문처리상태',
     dataIndex: 'status',
     key: 'status',
     render: StatusRenderer,
-    filters: [
-      {
-        text: '완료',
-        value: true,
-      },
-      {
-        text: '미완료',
-        value: false,
-      },
-    ],
-    filterMode: 'tree',
-    filterSearch: true,
-    onFilter: (value: string | number | boolean, record: ITableData) => record.status === value,
+    ...filterOptionObject
+
   },
   {
     title: '고객번호',
